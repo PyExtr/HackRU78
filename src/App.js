0a1697c51c94faf5e784fd1276c0cc91cc44e0cf
PyExtr <SimonDmitri.pr@gmail.com>
2023-05-11 19:08:25 +0300

    1
    libraries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   return (
     <div className="App">
+      {/* Google Web Fonts */}
       <header className="App-header">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +29,11 @@ function App() {
         >
           Learn React
         </a>
+        <div className="App">
+      <h1>Hello, World!</h1>
+      <p>This is my React component.</p>
+      <button>Click me</button>
+    </div>
       </header>
     </div>
   );
